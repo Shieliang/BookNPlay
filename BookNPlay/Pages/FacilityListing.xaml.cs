e833a31ed0491383a1f860ec3ff9a87f15bc23fd
Jason <jasonling0981@gmail.com>
2024-10-22 13:23:52 +0800

    facility

---
+++ BookNPlay/Pages/FacilityListing.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BookNPlay.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FacilityListing : ContentPage
+	{
+		public FacilityListing ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
