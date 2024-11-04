962437f448499797acfed390b06bc35eaaa0efd4
Jason <jasonling0981@gmail.com>
2024-11-04 18:26:08 +0800

    Facility Listing/Images

--- BookNPlay/Services/FacilityService.cs
+++ BookNPlay/Services/FacilityService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
 using BookNPlay.Models;
 
 namespace BookNPlay.Services
@@ -16,7 +17,7 @@ namespace BookNPlay.Services
                 new FacilityModel
                 {
                     Name = "Badminton Court",
-                    Date = "26/10/2024",
+                    Date = "26/11/2024",
                     Time = "10:00 AM - 12:00 PM",
                     ImageSource = "badminton.png",
                     IsAvailable = true
@@ -24,7 +25,7 @@ namespace BookNPlay.Services
                 new FacilityModel
                 {
                     Name = "Badminton Court",
-                    Date = "26/10/2024",
+                    Date = "26/11/2024",
                     Time = "2:00 PM - 4:00 PM",
                     ImageSource = "badminton.png",
                     IsAvailable = true
@@ -32,7 +33,7 @@ namespace BookNPlay.Services
                 new FacilityModel
                 {
                     Name = "Basketball Court",
-                    Date = "26/10/2024",
+                    Date = "26/11/2024",
                     Time = "1:00 PM - 3:00 PM",
                     ImageSource = "basketball.png",
                     IsAvailable = true
@@ -40,7 +41,7 @@ namespace BookNPlay.Services
                 new FacilityModel
                 {
                     Name = "Snooker Table",
-                    Date = "26/10/2024",
+                    Date = "27/11/2024",
                     Time = "11:00 AM - 1:00 PM",
                     ImageSource = "snooker.png",
                     IsAvailable = true
@@ -48,9 +49,25 @@ namespace BookNPlay.Services
                 new FacilityModel
                 {
                     Name = "Gym",
-                    Date = "26/10/2024",
+                    Date = "27/11/2024",
                     Time = "9:00 AM - 11:00 AM",
                     ImageSource = "gym.png",
+                    IsAvailable = true
+                },
+                new FacilityModel
+                {
+                    Name = "Netball Court",
+                    Date = "28/11/2024",
+                    Time = "3:00 PM - 5:00 PM",
+                    ImageSource = "netball.png",
+                    IsAvailable = true
+                },
+                new FacilityModel
+                {
+                    Name = "Football Field",
+                    Date = "29/11/2024",
+                    Time = "4:00 PM - 6:00 PM",
+                    ImageSource = "football.png",
                     IsAvailable = true
                 },
 
@@ -58,7 +75,7 @@ namespace BookNPlay.Services
                 new FacilityModel
                 {
                     Name = "Basketball Court",
-                    Date = "27/10/2024",
+                    Date = "26/11/2024",
                     Time = "9:00 AM - 11:00 AM",
                     ImageSource = "basketball.png",
                     IsAvailable = false
@@ -66,7 +83,7 @@ namespace BookNPlay.Services
                 new FacilityModel
                 {
                     Name = "Badminton Court",
-                    Date = "27/10/2024",
+                    Date = "27/11/2024",
                     Time = "2:00 PM - 4:00 PM",
                     ImageSource = "badminton.png",
                     IsAvailable = false
@@ -74,9 +91,33 @@ namespace BookNPlay.Services
                 new FacilityModel
                 {
                     Name = "Gym",
-                    Date = "27/10/2024",
-                    Time = "10:00 AM - 12:00 PM",
+                    Date = "27/11/2024",
+                    Time = "1:00 PM - 3:00 PM",
                     ImageSource = "gym.png",
+                    IsAvailable = false
+                },
+                new FacilityModel
+                {
+                    Name = "Netball Court",
+                    Date = "26/11/2024",
+                    Time = "1:00 PM - 3:00 PM",
+                    ImageSource = "netball.png",
+                    IsAvailable = false
+                },
+                new FacilityModel
+                {
+                    Name = "Netball Court",
+                    Date = "25/11/2024",
+                    Time = "4:00 PM - 6:00 PM",
+                    ImageSource = "netball.png",
+                    IsAvailable = false
+                },
+                new FacilityModel
+                {
+                    Name = "Football Field",
+                    Date = "25/11/2024",
+                    Time = "5:00 PM - 7:00 PM",
+                    ImageSource = "football.png",
                     IsAvailable = false
                 }
             };
@@ -85,13 +126,23 @@ namespace BookNPlay.Services
         public ObservableCollection<FacilityModel> GetAvailableFacilities()
         {
             return new ObservableCollection<FacilityModel>(
-                facilities.Where(f => f.IsAvailable));
+                facilities
+                .Where(f => f.IsAvailable)
+                .OrderBy(f => f.Name)
+                .ThenBy(f => DateTime.ParseExact(f.Date, "dd/MM/yyyy", null)) // 按日期排序
+                .ThenBy(f => DateTime.ParseExact(f.Time.Split(" - ")[0], "h:mm tt", null)) // 按时间排序
+            );
         }
 
         public ObservableCollection<FacilityModel> GetNotAvailableFacilities()
         {
             return new ObservableCollection<FacilityModel>(
-                facilities.Where(f => !f.IsAvailable));
+                facilities
+                .Where(f => !f.IsAvailable)
+                .OrderBy(f => f.Name)
+                .ThenBy(f => DateTime.ParseExact(f.Date, "dd/MM/yyyy", null)) // 按日期排序
+                .ThenBy(f => DateTime.ParseExact(f.Time.Split(" - ")[0], "h:mm tt", null)) // 按时间排序
+            );
         }
 
         public ObservableCollection<FacilityModel> SearchFacilities(string searchText, bool isAvailable)
