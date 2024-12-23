0cf84e517e43b1b1fac54c5e2d503b53d18c2d99
Shieliang <shieliang22@gmail.com>
2024-12-23 19:56:26 +0800

    Add Images

--- BookNPlay/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/BookNPlay.AssemblyInfo.cs
+++ BookNPlay/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/BookNPlay.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookNPlay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c0869b10ae7244fd86146a410b5d4ee97716a291")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookNPlay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookNPlay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
